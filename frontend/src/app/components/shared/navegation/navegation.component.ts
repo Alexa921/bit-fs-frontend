93838b68e385ea6235ef1a897d6dd8a3e0d0ad3f
Alexa921 <alexandrarestrepoagudelo451@gmail.com>
2025-03-10 21:17:24 -0500

    Actualizacion del proyecto

--- frontend/src/app/components/shared/navegation/navegation.component.ts
+++ frontend/src/app/components/shared/navegation/navegation.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-navigation',
+  selector: 'app-navegation',
   imports: [RouterLink],
   templateUrl: './navegation.component.html',
   styleUrl: './navegation.component.css',
